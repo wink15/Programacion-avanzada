d9e8020b737e6c50be2e3df13724db687a301ded
JoelDellamaggiore <56844660+JoelDellamaggiore@users.noreply.github.com>
2021-12-09 14:28:16 -0300

    BD nueva modificacion
    Se añade tabla:
    -ubicaciondelproyecto
    -moneda
    Se añade a la tabla "Proyecto" el atributo:
    -Monto DOUBLE(10,2)

--- BD_iteracion_2.sql
+++ BD_iteracion_2.sql
@@ -43,6 +43,30 @@ INSERT INTO `clientes` VALUES (1,'Leandro Martinez',2),(2,'Nicolas Rodriguez',1)
 UNLOCK TABLES;
 
 --
+-- Table structure for table `moneda`
+--
+
+DROP TABLE IF EXISTS `moneda`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `moneda` (
+  `idmoneda` int NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idmoneda`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `moneda`
+--
+
+LOCK TABLES `moneda` WRITE;
+/*!40000 ALTER TABLE `moneda` DISABLE KEYS */;
+INSERT INTO `moneda` VALUES (1,'Pesos'),(2,'Dolares');
+/*!40000 ALTER TABLE `moneda` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `nocliente`
 --
 
@@ -80,7 +104,7 @@ CREATE TABLE `perfil` (
   `nombre` varchar(45) DEFAULT NULL,
   `descripcion` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`idperfil`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -89,6 +113,7 @@ CREATE TABLE `perfil` (
 
 LOCK TABLES `perfil` WRITE;
 /*!40000 ALTER TABLE `perfil` DISABLE KEYS */;
+INSERT INTO `perfil` VALUES (1,'Perfil A','Este es el perfil x el cual consiste en A'),(2,'Perfil B','Este es el perfil B que consiste en'),(3,'Perfil C','Este es el perfil Y que consiste en'),(4,'Perfil D','Este es el perfil D que consiste en');
 /*!40000 ALTER TABLE `perfil` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -133,7 +158,7 @@ CREATE TABLE `personal` (
   PRIMARY KEY (`idPersonal`),
   KEY `fk_idPersona_personal` (`persona`),
   CONSTRAINT `fk_idPersona_personal` FOREIGN KEY (`persona`) REFERENCES `persona` (`id_persona`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -142,7 +167,7 @@ CREATE TABLE `personal` (
 
 LOCK TABLES `personal` WRITE;
 /*!40000 ALTER TABLE `personal` DISABLE KEYS */;
-INSERT INTO `personal` VALUES (10,20425110137,3);
+INSERT INTO `personal` VALUES (10,20425110137,3),(12,20423378094,1),(13,20414141414,2);
 /*!40000 ALTER TABLE `personal` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -158,7 +183,7 @@ CREATE TABLE `personal_perfil` (
   `personal` int DEFAULT NULL,
   `perfil` int DEFAULT NULL,
   PRIMARY KEY (`idpersonal_perfil`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -167,6 +192,7 @@ CREATE TABLE `personal_perfil` (
 
 LOCK TABLES `personal_perfil` WRITE;
 /*!40000 ALTER TABLE `personal_perfil` DISABLE KEYS */;
+INSERT INTO `personal_perfil` VALUES (1,10,1),(2,10,2),(3,10,3),(4,12,3);
 /*!40000 ALTER TABLE `personal_perfil` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -179,13 +205,14 @@ DROP TABLE IF EXISTS `proyecto`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `proyecto` (
   `idproyecto` int NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(45) DEFAULT NULL,
-  `fechaInicio` date DEFAULT NULL,
-  `fechaConfirmacion` date DEFAULT NULL,
-  `fechaFin` date DEFAULT NULL,
-  `tipoProyecto` int DEFAULT NULL,
-  `cliente` int DEFAULT NULL,
-  `observacion` varchar(200) DEFAULT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `fechaInicio` date NOT NULL,
+  `fechaConfirmacion` date NOT NULL,
+  `fechaFin` date NOT NULL,
+  `tipoProyecto` int NOT NULL,
+  `cliente` int NOT NULL,
+  `observacion` varchar(200) NOT NULL,
+  `monto` double(10,2) NOT NULL,
   PRIMARY KEY (`idproyecto`)
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -196,7 +223,7 @@ CREATE TABLE `proyecto` (
 
 LOCK TABLES `proyecto` WRITE;
 /*!40000 ALTER TABLE `proyecto` DISABLE KEYS */;
-INSERT INTO `proyecto` VALUES (1,'Proyecto n° 1','2021-09-22','2021-09-22','2021-09-22',0,0,'Proyecto terminado exitosamente!'),(4,'Proyecto n° 2','2021-09-22','2021-09-21','2021-09-23',1,1,'Proyecto terminado exitosamente'),(5,'Proyecto n° 3','2021-09-22','2021-09-21','2021-09-23',1,1,'Proyecto terminado exitosamente'),(7,'Proyecto n° 4','2021-09-23','2021-09-22','2021-09-24',1,0,'nProyecto terminado exitosamente'),(8,'Proyecto n° 5','2021-09-22','2021-09-21','2021-09-23',1,1,'Proyecto terminado exitosamente'),(9,'Proyecto n° 6','2021-09-29','2021-09-28','2021-09-30',6,1,'Proyecto en curso'),(11,'Proyecto n° 7','2021-09-29','2021-09-28','2021-09-30',6,2,'Proyecto en curso'),(12,'Proyecto n° 8','2021-09-29','2021-09-28','2021-09-30',2,1,'Proyecto en curso!');
+INSERT INTO `proyecto` VALUES (1,'Proyecto n° 1','2021-09-22','2021-09-22','2021-09-22',0,0,'Proyecto terminado exitosamente!',0.00),(4,'Proyecto n° 2','2021-09-22','2021-09-21','2021-09-23',1,1,'Proyecto terminado exitosamente',0.00),(5,'Proyecto n° 3','2021-09-22','2021-09-21','2021-09-23',1,1,'Proyecto terminado exitosamente',0.00),(7,'Proyecto n° 4','2021-09-23','2021-09-22','2021-09-24',1,0,'nProyecto terminado exitosamente',0.00),(8,'Proyecto n° 5','2021-09-22','2021-09-21','2021-09-23',1,1,'Proyecto terminado exitosamente',0.00),(9,'Proyecto n° 6','2021-09-29','2021-09-28','2021-09-30',6,1,'Proyecto en curso',0.00),(11,'Proyecto n° 7','2021-09-29','2021-09-28','2021-09-30',6,2,'Proyecto en curso',0.00),(12,'Proyecto n° 8','2021-09-29','2021-09-28','2021-09-30',2,1,'Proyecto en curso!',0.00);
 /*!40000 ALTER TABLE `proyecto` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -212,7 +239,7 @@ CREATE TABLE `proyecto_perfil` (
   `proyecto` int DEFAULT NULL,
   `perfil` int DEFAULT NULL,
   PRIMARY KEY (`idproyecto_perfil`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -221,6 +248,7 @@ CREATE TABLE `proyecto_perfil` (
 
 LOCK TABLES `proyecto_perfil` WRITE;
 /*!40000 ALTER TABLE `proyecto_perfil` DISABLE KEYS */;
+INSERT INTO `proyecto_perfil` VALUES (1,5,1),(2,5,2),(3,5,3),(4,9,1),(5,7,2);
 /*!40000 ALTER TABLE `proyecto_perfil` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -248,6 +276,32 @@ LOCK TABLES `tipo_proyecto` WRITE;
 INSERT INTO `tipo_proyecto` VALUES (1,'ciencia','Tipo de proyecto asociado a las ciencias sociales y naturales '),(6,'matematica','Tipo de proyecto asociado a las matematicas'),(9,'Mecanicaa','Tipo de proceso asociado a la mecanica automotriz ');
 /*!40000 ALTER TABLE `tipo_proyecto` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `ubicaciondelproyecto`
+--
+
+DROP TABLE IF EXISTS `ubicaciondelproyecto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ubicaciondelproyecto` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `descripcion` varchar(45) NOT NULL,
+  `moneda` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ubicaciondelproyecto`
+--
+
+LOCK TABLES `ubicaciondelproyecto` WRITE;
+/*!40000 ALTER TABLE `ubicaciondelproyecto` DISABLE KEYS */;
+INSERT INTO `ubicaciondelproyecto` VALUES (1,'Nacional','Proyecto realizado en Argentina',1),(2,'Exterior','Proyecto realizado fuera de Argentina',2);
+/*!40000 ALTER TABLE `ubicaciondelproyecto` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -258,4 +312,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-10-31 20:51:02
+-- Dump completed on 2021-12-09 14:21:47
